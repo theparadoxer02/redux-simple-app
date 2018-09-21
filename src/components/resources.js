d3e9a40dfd124f6a55375963cecc21114b98a728
Mannu Gupta <abhimanyu98986@gmail.com>
2018-09-21 15:13:00 +0530

    completed project

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+	return (
+		<div>
+			Super Special Recipe
+			<ul>
+				<li> 1 Cup Sugar </li>
+				<li></li>
+				<li></li>
+			</ul>
+		</div>
+	);
+};
